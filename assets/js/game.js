61474884dfd10eeb50002fb04d16995b125ea6fa
Booblays <samuelaaronsook@gmail.com>
2022-01-30 13:43:44 -0800

    null name bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,9 +5,18 @@ var randomNumber = function(min, max) {
           return value;
 }
 
+var getPlayerName = function() {
+     var name = "";
+     while (name === "" || name === null) {
+         name = prompt("What is your robot's name?"); 
+     }
+     
+
+     console.log("Your robot's name is " + name)
+}
 
 var playerInfo = {
-     name: window.prompt("What is yout robot's name?"),
+     name: getPlayerName(),   
      health: 100,
      attack: 10,
      money: 10,
