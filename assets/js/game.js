d5a9988cec0453c689dbc756be45b83b64f9cf1a
Booblays <samuelaaronsook@gmail.com>
2022-01-30 19:12:21 -0800

    shop int input

--- assets/js/game.js
+++ assets/js/game.js
@@ -162,29 +162,28 @@ var endGame = function() {
      // Shop Function
 var shop = function() {
      var shopOptionPrompt = window.prompt(
-          "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+          "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
      );
+     
+     shopOptionPrompt = parseInt(shopOptionPrompt);
 
      switch (shopOptionPrompt) {
-          case "REFILL":
-          case "refill":
+          case 1:
                playerInfo.refillHealth();
                break;
           
-          case "UPGRADE":
-          case "upgrade":
-               playerInfo.refillHealth();
+          case 2:
+               playerInfo.refillAttack();
                break;
 
-          case "LEAVE":
-          case "leave":
+          case 3:
                window.alert("Leaving the store.");
           break;
 
           default:
                window.alert("You did not pick a valid option. Try again.");
                shop();
-          break;
+               break;
      }
 };
 
