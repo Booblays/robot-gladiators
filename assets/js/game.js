a9a7d2d84368188a7a7fd789885486d053c09368
Booblays <samuelaaronsook@gmail.com>
2022-01-31 14:07:47 -0800

    high score keeping

--- assets/js/game.js
+++ assets/js/game.js
@@ -155,11 +155,21 @@ var fight = function(enemy) {
      // Endgame Function
 var endGame = function() {
 
-     if (playerInfo.health > 0) {
-          window.alert("The game has now ended. Let's see how you did!");
+     window.alert("The game has now ended. Let's see how you did!");
+
+     var highScore = localStorage.getItem("high score");
+     if (highScore === null) {
+          highScore = 0;
+     }
+
+     if (playerInfo.money > highScore) {
+          localStorage.setItem("highscore", playerInfo.money);
+          localStorage.setItem("name", playerInfo.name);
+
+          alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
      }
      else {
-          window.alert("You've lost your robot in battle.")
+          window.alert(playerInfo.name + " did not beat the high score " + highScore + ". Maybe next time!");
      }
 
      var playAgainConfirm = window.confirm("Would you like to play again?");
